56c7bb380db3a34ec2af9bc700d25e184281b464
cloudrack-ca <145787423+cloudrack-ca@users.noreply.github.com>
2023-09-23 19:59:59 -0400

    Update wp-bash.sh

--- wp-bash.sh
+++ wp-bash.sh
@@ -3,9 +3,12 @@
 # This script is meant to be run as root
 #!/bin/bash
 
-# Install required packages
+# Add PHP 8.1 repository
+add-apt-repository ppa:ondrej/php
 apt-get update
-apt-get install -y apache2 mysql-server php libapache2-mod-php php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip curl
+
+# Install required packages
+apt-get install -y apache2 mysql-server php8.1 libapache2-mod-php8.1 php8.1-mysql php8.1-curl php8.1-gd php8.1-mbstring php8.1-xml php8.1-xmlrpc php8.1-soap php8.1-intl php8.1-zip curl
 
 # Download and extract WordPress
 curl -O https://wordpress.org/latest.tar.gz
@@ -20,11 +23,11 @@ chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
 # Set PHP.ini settings
-mkdir -p /etc/php/7.2/apache2/
-touch /etc/php/7.2/apache2/php.ini
-sed -i 's/memory_limit = .*/memory_limit = 256M/' /etc/php/7.2/apache2/php.ini
-sed -i 's/upload_max_filesize = .*/upload_max_filesize = 64M/' /etc/php/7.2/apache2/php.ini
-sed -i 's/post_max_size = .*/post_max_size = 64M/' /etc/php/7.2/apache2/php.ini
+mkdir -p /etc/php/8.1/apache2/
+touch /etc/php/8.1/apache2/php.ini
+sed -i 's/memory_limit = .*/memory_limit = 256M/' /etc/php/8.1/apache2/php.ini
+sed -i 's/upload_max_filesize = .*/upload_max_filesize = 64M/' /etc/php/8.1/apache2/php.ini
+sed -i 's/post_max_size = .*/post_max_size = 64M/' /etc/php/8.1/apache2/php.ini
 
 # Restart Apache
-ls /etc/php/7.2/apache2/php.ini
+systemctl restart apache2
