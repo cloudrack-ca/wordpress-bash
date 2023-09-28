51462edc7c1e5d1a3ae3b6e068772c5425ac9482
Cloudrack.ca <145787423+cloudrack-ca@users.noreply.github.com>
2023-09-28 13:37:22 -0400

    Update wp-bash.sh

--- wp-bash.sh
+++ wp-bash.sh
@@ -11,45 +11,46 @@ NC='\033[0m' # No Color
 
 # Get localhost IP
 ip=$(hostname -I | awk '{print $1}')
-
+# Tell the user "Lets start installing your WordPress site" in Purple
+echo -e "${BLUE}Lets start installing your WordPress site simply follow the steps to install.${NC}"
 # Prompt user to define PHP version
-echo -e "${YELLOW}Enter PHP version (e.g. 8.1) [default: 8.1]: ${NC}"
+echo -e "${YELLOW}Enter PHP version (e.g. 8.1) [If none is entered default will be used: 8.1]: ${NC}"
 read -p "(default: 8.1) " php_version
 php_version=${php_version:-8.1}
 echo -e "${GREEN}Using PHP version: $php_version${NC}"
 
 # Prompt user to define WordPress version
-echo -e "${YELLOW}Enter WordPress version (e.g. latest) [default: latest]: ${NC}"
+echo -e "${YELLOW}Enter WordPress version (e.g. latest) [If none is entered default will be used: latest]: ${NC}"
 read -p "(default: latest) " wp_version
 wp_version=${wp_version:-latest}
 echo -e "${GREEN}Using WordPress version: $wp_version${NC}"
 
 # Prompt user to define WordPress directory
-echo -e "${YELLOW}Enter WordPress directory (e.g. /var/www/html/wordpress) [default: /var/www/html/wordpress]: ${NC}"
+echo -e "${YELLOW}Enter WordPress directory (e.g. /var/www/html/wordpress) [If none is entered default will be used: /var/www/html/wordpress]: ${NC}"
 read -p "(default: /var/www/html/wordpress) " wp_dir
 wp_dir=${wp_dir:-/var/www/html/wordpress}
 echo -e "${GREEN}Using WordPress directory: $wp_dir${NC}"
 
 # Prompt user to define MySQL root password
-echo -e "${YELLOW}Enter MySQL root password [default: root]: ${NC}"
+echo -e "${YELLOW}Enter MySQL root password [If none is entered default will be used: root]: ${NC}"
 read -s -p "(default: root) " mysql_root_password
 mysql_root_password=${mysql_root_password:-root}
 echo -e "${GREEN}Using MySQL root password: $mysql_root_password${NC}"
 
 # Prompt user to define WordPress database name
-echo -e "${YELLOW}Enter WordPress database name [default: wordpress_db]: ${NC}"
+echo -e "${YELLOW}Enter WordPress database name [If none is entered default will be used: wordpress_db]: ${NC}"
 read -p "(default: wordpress_db) " wp_db_name
 wp_db_name=${wp_db_name:-wordpress_db}
 echo -e "${GREEN}Using WordPress database name: $wp_db_name${NC}"
 
 # Prompt user to define WordPress database user
-echo -e "${YELLOW}Enter WordPress database user [default: wordpress_user]: ${NC}"
+echo -e "${YELLOW}Enter WordPress database user [If none is entered default will be used: wordpress_user]: ${NC}"
 read -p "(default: wordpress_user) " wp_db_user
 wp_db_user=${wp_db_user:-wordpress_user}
 echo -e "${GREEN}Using WordPress database user: $wp_db_user${NC}"
 
 # Prompt user to define WordPress database user password
-echo -e "${YELLOW}Enter WordPress database user password [default: password]: ${NC}"
+echo -e "${YELLOW}Enter WordPress database user password [If none is entered default will be used: password]: ${NC}"
 read -s -p "(default: password) " wp_db_password
 wp_db_password=${wp_db_password:-password}
 echo -e "${GREEN}Using WordPress database user password: $wp_db_password${NC}"
@@ -106,12 +107,19 @@ sed -i 's/post_max_size = .*/post_max_size = 64M/' /etc/php/$php_version/apache2
 # Restart Apache
 echo -e "${GREEN}Restarting Apache...${NC}"
 systemctl restart apache2 &>> install.log
-
+# Tada Your WordPress installation is complete in cyan
+echo -e "${BLUE}🎉 Tada 🎉 Your WordPress installation is complete${NC}"
 # Display user's database info
 echo -e "${BLUE}Your WordPress database information:${NC}"
-echo -e "${YELLOW}Database name:${NC} $wp_db_name"
-echo -e "${YELLOW}Database user:${NC} $wp_db_user"
-echo -e "${YELLOW}Database user password:${NC} $wp_db_password"
+echo -e "${YELLOW}Please dont lose this - Your Database name:${NC} $wp_db_name"
+echo -e "${YELLOW}Please dont lose this - Your Database user:${NC} $wp_db_user"
+echo -e "${YELLOW}Please dont lose this - Your Database user password:${NC} $wp_db_password"
 
 # Display localhost IP
-echo -e "${BLUE}Your localhost IP:${NC} $ip"
+echo -e "${BLUE}your wordpress installation will be on your IP located -> :${NC} $ip"
+# Display wordpress directory
+echo -e "${BLUE}your wordpress installation will be on your IP located -> :${NC} $wp_dir"
+# Display PHP version
+echo -e "${BLUE}your wordpress installation will be on your IP located -> :${NC} $php_version"
+# Display WordPress version
+echo -e "${BLUE}your wordpress installation will be on your IP located -> :${NC} $wp_version"
