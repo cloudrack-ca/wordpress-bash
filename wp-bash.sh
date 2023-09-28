7d4fb3d14ad1a0a0bdc68f12602c48122c1508f9
Cloudrack.ca <145787423+cloudrack-ca@users.noreply.github.com>
2023-09-28 13:18:46 -0400

    Update wp-bash.sh

--- wp-bash.sh
+++ wp-bash.sh
@@ -9,17 +9,50 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
+# Get localhost IP
+ip=$(hostname -I | awk '{print $1}')
+
 # Prompt user to define PHP version
-echo -e "${YELLOW}Enter PHP version (e.g. 8.1): ${NC}"
-read php_version
+echo -e "${YELLOW}Enter PHP version (e.g. 8.1) [default: 8.1]: ${NC}"
+read -p "(default: 8.1) " php_version
+php_version=${php_version:-8.1}
+echo -e "${GREEN}Using PHP version: $php_version${NC}"
 
 # Prompt user to define WordPress version
-echo -e "${YELLOW}Enter WordPress version (e.g. latest): ${NC}"
-read wp_version
+echo -e "${YELLOW}Enter WordPress version (e.g. latest) [default: latest]: ${NC}"
+read -p "(default: latest) " wp_version
+wp_version=${wp_version:-latest}
+echo -e "${GREEN}Using WordPress version: $wp_version${NC}"
 
 # Prompt user to define WordPress directory
-echo -e "${YELLOW}Enter WordPress directory (e.g. /var/www/html/wordpress): ${NC}"
-read wp_dir
+echo -e "${YELLOW}Enter WordPress directory (e.g. /var/www/html/wordpress) [default: /var/www/html/wordpress]: ${NC}"
+read -p "(default: /var/www/html/wordpress) " wp_dir
+wp_dir=${wp_dir:-/var/www/html/wordpress}
+echo -e "${GREEN}Using WordPress directory: $wp_dir${NC}"
+
+# Prompt user to define MySQL root password
+echo -e "${YELLOW}Enter MySQL root password [default: root]: ${NC}"
+read -s -p "(default: root) " mysql_root_password
+mysql_root_password=${mysql_root_password:-root}
+echo -e "${GREEN}Using MySQL root password: $mysql_root_password${NC}"
+
+# Prompt user to define WordPress database name
+echo -e "${YELLOW}Enter WordPress database name [default: wordpress_db]: ${NC}"
+read -p "(default: wordpress_db) " wp_db_name
+wp_db_name=${wp_db_name:-wordpress_db}
+echo -e "${GREEN}Using WordPress database name: $wp_db_name${NC}"
+
+# Prompt user to define WordPress database user
+echo -e "${YELLOW}Enter WordPress database user [default: wordpress_user]: ${NC}"
+read -p "(default: wordpress_user) " wp_db_user
+wp_db_user=${wp_db_user:-wordpress_user}
+echo -e "${GREEN}Using WordPress database user: $wp_db_user${NC}"
+
+# Prompt user to define WordPress database user password
+echo -e "${YELLOW}Enter WordPress database user password [default: password]: ${NC}"
+read -s -p "(default: password) " wp_db_password
+wp_db_password=${wp_db_password:-password}
+echo -e "${GREEN}Using WordPress database user password: $wp_db_password${NC}"
 
 # Add PHP repository
 echo -e "${BLUE}Adding PHP $php_version repository...${NC}"
@@ -32,6 +65,13 @@ apt-get update &>> install.log
 # Install required packages
 echo -e "${BLUE}Installing required packages...${NC}"
 apt-get install -y apache2 mysql-server php$php_version libapache2-mod-php$php_version php$php_version-mysql php$php_version-curl php$php_version-gd php$php_version-mbstring php$php_version-xml php$php_version-xmlrpc php$php_version-soap php$php_version-intl php$php_version-zip curl &>> install.log
+
+# Create WordPress database and user
+echo -e "${BLUE}Creating WordPress database and user...${NC}"
+mysql -u root -p$mysql_root_password -e "CREATE DATABASE $wp_db_name;"
+mysql -u root -p$mysql_root_password -e "CREATE USER '$wp_db_user'@'localhost' IDENTIFIED BY '$wp_db_password';"
+mysql -u root -p$mysql_root_password -e "GRANT ALL PRIVILEGES ON $wp_db_name.* TO '$wp_db_user'@'localhost';"
+mysql -u root -p$mysql_root_password -e "FLUSH PRIVILEGES;"
 
 # Download and extract WordPress
 echo -e "${BLUE}Downloading and extracting WordPress $wp_version...${NC}"
@@ -66,3 +106,12 @@ sed -i 's/post_max_size = .*/post_max_size = 64M/' /etc/php/$php_version/apache2
 # Restart Apache
 echo -e "${GREEN}Restarting Apache...${NC}"
 systemctl restart apache2 &>> install.log
+
+# Display user's database info
+echo -e "${BLUE}Your WordPress database information:${NC}"
+echo -e "${YELLOW}Database name:${NC} $wp_db_name"
+echo -e "${YELLOW}Database user:${NC} $wp_db_user"
+echo -e "${YELLOW}Database user password:${NC} $wp_db_password"
+
+# Display localhost IP
+echo -e "${BLUE}Your localhost IP:${NC} $ip"
